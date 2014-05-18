130a98f2c4d679af59082fee03acb4f04b5ca394
Mike Fitzgerald <mike@xpressivecode.com>
2014-05-18 00:13:25 -0400

    ability to add time and have it submit to assembla

--- routes/spaces.js
+++ routes/spaces.js
@@ -1,7 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var moment = require('moment');
 var sa = require('superagent');
+
+router.get('/tasks', function(req, res){
+    var base_url = 'https://api.assembla.com/v1/';
+
+    var api_key = req.session.user.api_key;
+    var api_secret = req.session.user.api_secret;
+
+    sa.get(base_url + 'tasks')
+        .set('X-Api-Key', api_key)
+        .set('X-Api-Secret', api_secret)
+        .end(function(err, response){
+            return res.json(response.body);
+        });
+
+});
 
 router.get('/:id', function(req, res){
     var base_url = 'https://api.assembla.com/v1/';
@@ -42,6 +57,41 @@ router.get('/:id/tickets/:ticket_id', function(req, res){
 
             return res.render('ticket', { space_id: req.params.id, ticket: response.body });
         });
+});
+
+router.post('/:id/tickets/:ticket_id', function(req, res){
+    var base_url = 'https://api.assembla.com/v1/';
+
+    var api_key = req.session.user.api_key;
+    var api_secret = req.session.user.api_secret;
+
+    sa.post(base_url + 'tasks')
+        .send({ 'user_task': {
+                'user_id': req.session.user.id,
+                'space_id': req.params.id,
+                'description': 'auto added time',
+                'hours': req.body.hours,
+                'begin_at': moment(req.body.start_date).toJSON(),
+                'end_at': moment().toJSON(),
+                'ticket_id': req.params.ticket_id
+            }
+        })
+        .set('Content-type', 'application/json')
+        .set('X-Api-Key', api_key)
+        .set('X-Api-Secret', api_secret)
+        .end(function(err, response){
+            if(err)return res.send(500, err);
+
+            console.log('ticket: ', response.body);
+
+            req.session.notification = {
+                type: 'success',
+                title: 'Time Entry has been Added'
+            };
+
+            return res.redirect('/spaces/' + req.params.id + '/tickets/' + req.params.ticket_id);
+        });
+
 });
 
 module.exports = router;
