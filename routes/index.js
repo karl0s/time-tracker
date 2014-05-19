aa00884151f9ee092bb5faa86acd945dbbb00fab
Mike Fitzgerald <mike@xpressivecode.com>
2014-05-19 11:21:23 -0400

    iframe in the calendar so that you can share it now too

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,51 @@ router.get('/', function(req, res) {
     return res.render('login');
 });
 
+router.get('/calendar/view/:key/:secret', function(req, res){
+    var base_url = 'https://api.assembla.com/v1/';
+
+    var api_key = req.params.key;
+    var api_secret = req.params.secret;
+
+    sa.get(base_url + '/spaces')
+        .set('X-Api-Key', api_key)
+        .set('X-Api-Secret', api_secret)
+        .end(function(err, response){
+            var spaces = response.body;
+
+            sa.get(base_url + '/spaces/' + spaces[0].id + '/tickets/my_active.json')
+                .set('X-Api-Key', api_key)
+                .set('X-Api-Secret', api_secret)
+                .end(function(err, response){
+                    if(err)return res.send(500, err);
+
+                    var tickets = response.body;
+                    var events = [];
+
+                    tickets.forEach(function(ticket){
+                        if(ticket.custom_fields && ticket.custom_fields["Due Date"]) {
+                            var start_date = moment(ticket.custom_fields["Due Date"]);
+                            var end_date = start_date.add('h', 1);
+
+                            events.push({
+                                title: ticket.summary,
+                                id: ticket.id,
+                                start: start_date.toDate(),
+                                end: end_date.toDate(),
+                                url: 'https://assembla.com/spaces/' + ticket.space_id + '/tickets/' + ticket.number
+                            });
+                        }
+                    });
+
+                    return res.render('shared/_calendar', {
+                        events: events,
+                        api_key: api_key,
+                        api_secret: api_secret
+                    });
+                });
+        });
+});
+
 router.get('/calendar', function(req, res){
     var base_url = 'https://api.assembla.com/v1/';
 
@@ -103,7 +148,6 @@ router.get('/calendar/:key/:secret', function(req, res){
                         }
                     });
 
-                    return res.send(cal.toString());
                     res.setHeader('Content-type', 'text/calendar; charset=utf-8');
                     return res.send(cal.toString());
                 });
